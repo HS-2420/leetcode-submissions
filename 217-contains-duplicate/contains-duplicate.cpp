71aa6a94bd0a52ddc2d73808d1c9b56f5a7c3c41
Himanshu <150070624+HS-2420@users.noreply.github.com>
2024-05-17 00:37:18 +0530

    Time: 87 ms (75.23%) | Memory: 60.7 MB (92.68%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -5,9 +5,7 @@ public:
       for(int i=0;i<nums.size()-1;i++)
       {
           if(nums[i]==nums[i+1])
-          {
-          return true;
-          }
+          { return true; }
       }
       return false;
     }
