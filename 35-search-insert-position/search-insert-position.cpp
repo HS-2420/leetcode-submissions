9e33dca0be612bf7dc8f7584297ae4de80c5f564
Himanshu <150070624+HS-2420@users.noreply.github.com>
2024-05-20 16:31:09 +0530

    Time: 7 ms (22.15%) | Memory: 12.1 MB (45.17%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,17 +4,6 @@ public:
         int s=0;
         int n=nums.size();
         int e=n-1;
-        // while(s<=e)
-        // {
-        //     int m= s + ((e-s)/2);
-        //     if(nums[m]==target)
-        //     { return m; }
-        //     else if(target>nums[m])
-        //     { s=m+1; }
-        //     else
-        //     { e=m-1; }
-        // }
-        // return s;
         if(target>nums[e])
             {return n;}
         while(s<e)
@@ -22,9 +11,8 @@ public:
             int m=s+((e-s)/2);
             if(nums[m]>=target)
             {e=m;}
-            else{
-                s=m+1;
-            }
+            else
+            { s=m+1; }
         }
         return s;
     }
