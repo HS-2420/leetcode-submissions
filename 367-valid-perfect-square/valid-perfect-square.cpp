d4966dd8178daebd898cfddceae50c3e96921771
Himanshu <150070624+HS-2420@users.noreply.github.com>
2024-05-21 13:28:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (55.19%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
+int findSqrt(int num)
+{
+    int s=1;int e=num;
+    while(s<=e)
+    {
+        long long m=s+((e-s)/2);
+        long long val=m*m;
+        if(val>num)
+        {e=m-1;}
+        else
+        {s=m+1;}
+    }
+    return e;
+}
     bool isPerfectSquare(int num) {
-        int ans=sqrt(num);
+        int ans=findSqrt(num);
         if(ans*ans==num)
         {
             return true;
