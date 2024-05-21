8c48f5cb92a6ef0a1ff9b593b76386d9598d3528
Himanshu <150070624+HS-2420@users.noreply.github.com>
2024-05-21 12:22:11 +0530

    Time: 11 ms (97.16%) | Memory: 24.5 MB (85.18%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-           int s = 0;
+        int s = 0;
         int e = nums.size() - 1;
-        
-        while (s < e) {
+        while (s < e) 
+        {
             int m = s + (e - s) / 2;
-            
-            // Ensure m is even for checking pairs
-            if (m % 2 == 1) {
-                m--;
-            }
-            
-            // Compare the pair elements
-            if (nums[m] == nums[m + 1]) {
-                // Single element is in the right half
-                s = m + 2;
-            } else {
-                // Single element is in the left half, including m
-                e = m;
-            }
+            if (m % 2 == 1) 
+            { m--; }
+            if (nums[m] == nums[m + 1])
+             { s = m + 2; }
+             else 
+            { e = m; }
         }
-        
         return nums[s];
     }
 };
