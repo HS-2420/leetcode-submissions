062847231cf1be70c0e9791822c4da30c60ad2c2
Himanshu <150070624+HS-2420@users.noreply.github.com>
2024-05-22 13:19:23 +0530

    Time: 52 ms (5.82%) | Memory: 33.5 MB (25.01%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -16,11 +16,20 @@
         }
         return day<=days;
     }
+    int findSum(vector<int>& weights)
+    {
+        int sum=0;
+        for(int i=0;i<weights.size();i++)
+        {
+            sum=sum+weights[i];
+        }
+        return sum;
+    }
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {  
         int s=1;
-        int e=accumulate(weights.begin(),weights.end(),0);
+        int e=findSum(weights);
         while(s<e){
             int mid=s+((e-s)/2);
             if(check(weights,mid,days))
