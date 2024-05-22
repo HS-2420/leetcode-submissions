38869bcbf35135b142e698dad18744dc38da645f
Himanshu <150070624+HS-2420@users.noreply.github.com>
2024-05-22 13:11:55 +0530

    Time: 40 ms (53.22%) | Memory: 33.3 MB (97.86%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool check(vector<int> &wts,int w,int days)
+    {
+        int day=1;
+        int wt=0;
+        for(int i=0;i<wts.size();i++)
+        {
+            if(wt+wts[i]>w)
+            {
+                wt=wts[i];
+                if(wt>w)
+                { return 0; }
+                day++;
+            }
+            else
+            { wt+=wts[i]; }
+        }
+        return day<=days;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {  
+        int s=1;
+        int e=accumulate(weights.begin(),weights.end(),0);
+        int ans=-1;
+        while(s<=e){
+            int mid=s+((e-s)/2);
+            if(check(weights,mid,days))
+            {
+                ans=mid;
+                e=mid-1;
+            }
+            else
+            { s=mid+1; }
+        }
+        return ans;
+    }
+};
