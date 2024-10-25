82d0bb3d268d66833efac8badf6d4237bf2f677a
Himanshu <150070624+HS-2420@users.noreply.github.com>
2024-10-25 23:42:17 +0530

    Time: 95 ms (46.34%) | Memory: 76.2 MB (5.39%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -8,12 +8,22 @@ public:
     //     s.insert(num); }
     // return false;
 
-     unordered_set<int>s;
-        for (int num : nums) {
-            if (s.count(num) > 0)
-                return true;
-            s.insert(num);
-        }
-        return false;
+    //  unordered_set<int>s;
+    //     for (int num : nums) {
+    //         if (s.count(num) > 0)
+    //             return true;
+    //         s.insert(num);
+    //     }
+    //     return false;
+
+    set<int>s;
+    for(auto num:nums)
+    {
+        s.insert(num);
+    }
+    if(s.size()==nums.size())
+    { return false; }
+    else
+    {return true;}
     }
 };
