2c00202a7d57a467c47e126a98f150b422fc52ca
Himanshu <150070624+HS-2420@users.noreply.github.com>
2024-10-28 01:58:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (54.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s==t;
+        if(s.length() != t.length()){
+            return false;
+        }
+        int hash[26]={0};
+        for(auto H : s)
+        {
+            hash[H - 'a']++;
+        }
+        for(auto H : t)
+        {
+            hash[H - 'a']--;
+        }
+        for(auto val:hash)
+        {
+            if(val != 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };
