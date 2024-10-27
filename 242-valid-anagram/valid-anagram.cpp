2f29468b3a69e46bb66c292b58dc2a4df4465471
Himanshu <150070624+HS-2420@users.noreply.github.com>
2024-10-28 01:59:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (54.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,24 +4,17 @@ public:
         // sort(s.begin(),s.end());
         // sort(t.begin(),t.end());
         // return s==t;
-        if(s.length() != t.length()){
-            return false;
-        }
+        if(s.length() != t.length())
+        { return false; }
         int hash[26]={0};
         for(auto H : s)
-        {
-            hash[H - 'a']++;
-        }
+        { hash[H - 'a']++; }
         for(auto H : t)
-        {
-            hash[H - 'a']--;
-        }
+        { hash[H - 'a']--; }
         for(auto val:hash)
         {
             if(val != 0)
-            {
-                return false;
-            }
+            { return false; }
         }
         return true;
     }
