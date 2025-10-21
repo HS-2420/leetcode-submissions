8b66b4afb31656701685b159bd3946a9d676f5e0
Himanshu <150070624+HS-2420@users.noreply.github.com>
2025-10-21 16:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (80.39%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -19,14 +19,11 @@ public:
         //         ans.push_back(nums[j]);
         //     }
         // }
-
-
         vector<int> ans(2*nums.size());
         for(int i=0;i<nums.size();i++){
             ans[i]=nums[i];
             ans[nums.size()+i]=nums[i];
         } 
-
         return ans;
     }
 };
