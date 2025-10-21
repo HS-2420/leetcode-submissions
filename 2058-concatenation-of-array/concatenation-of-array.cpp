26d6a0ea01ee3771ebea7cfc1e5466eb9bec1fe8
Himanshu <150070624+HS-2420@users.noreply.github.com>
2025-10-21 17:07:26 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (80.39%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -1,29 +1,12 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        //      int n = nums.size();
-        // vector<int> ans(2 * n);
-        // for (int i = 0; i < n; ++i) {
-        //     ans[i] = nums[i];
-        // }
-        // for (int i = n; i < 2 * n; ++i) {
-        //     ans[i] = nums[i - n];
-        // }   
-        // return ans;
-
-        // vector<int>ans;
-        // for(int i=0;i<2;i++)
-        // {
-        //     for(int j=0;j<nums.size();j++)
-        //     {
-        //         ans.push_back(nums[j]);
-        //     }
-        // }
-        vector<int> ans(2*nums.size());
+        vector<int> new_arr(2*nums.size());
         for(int i=0;i<nums.size();i++){
-            ans[i]=nums[i];
-            ans[nums.size()+i]=nums[i];
-        } 
-        return ans;
+        int num= nums[i];
+        new_arr[i]=num;
+        new_arr[i+nums.size()]=num;
+        }
+        return new_arr;
     }
 };
