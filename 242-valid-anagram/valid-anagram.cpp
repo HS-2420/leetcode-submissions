cdc32ad98acb3a88c8267da83635d1d265f32b34
Himanshu <150070624+HS-2420@users.noreply.github.com>
2025-10-22 12:19:14 +0530

    Time: 2 ms (56.07%) | Memory: 9.8 MB (48.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
-    //     if(s.length() != t.length())
-    //     { return false; }
-    //     int hash[26]={0};
-    //     for(auto H : s)
-    //     { hash[H - 'a']++; }
-    //     for(auto H : t)
-    //     { hash[H - 'a']--; }
-    //     for(auto val:hash)
-    //     {
-    //         if(val != 0)
-    //         { return false; }
-    //     }
-    //     return true;
+         if(s.length() != t.length()){
+            return false;}
+        unordered_map<char,int>count;
+        for(char ch:s){
+            count[ch]++;
+        }
+        for(char ch:t){
+            count[ch]--;
+        }
+        for(auto i:count){
+            if(i.second!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
